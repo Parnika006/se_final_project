ba5bea7f44c3a9aeeff1f4ba2b0da99ca4f43167
Parnika006 <parnikasingh006@gmail.com>
2025-02-09 19:27:02 -0800

    update in vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
-  },
-  build: {
-    outDir: "dist", // Ensures output to the 'dist' folder
-    rollupOptions: {
-      input: "index.html", // Main entry point, ensure this is correct
-    },
   },
 });
