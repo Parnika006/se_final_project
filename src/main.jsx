e8ddc886a9dba8732573231114a6f4a23a4ec2d2
Parnika006 <parnikasingh006@gmail.com>
2025-02-10 10:28:58 -0800

    gh-pages branch updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
+    <HashRouter
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true,
       }}
     >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/se_final_project",
   plugins: [react()],
   server: {
     port: 3000,
