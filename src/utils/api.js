7431562dd9402e3fa68a46212fa8ad13458ff2c3
Parnika006 <parnikasingh006@gmail.com>
2025-02-10 10:19:21 -0800

    Updates

--- src/utils/api.js
+++
@@ -1,3 +0,0 @@
-export const handleServerResponse = (res) => {
-  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-};
--- src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const apiKey = "8a3a50e8b4184b3a8a4e7675c85f14ce";
