3ed7e79108715b58b209f14abeebe333b142c27e
Parnika006 <parnikasingh006@gmail.com>
2025-03-03 17:19:20 -0800

    baseUrl and package.json update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
 export const apiKey = "8a3a50e8b4184b3a8a4e7675c85f14ce";
 
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.newex.crabdance.com"
+    : "http://localhost:3001";
